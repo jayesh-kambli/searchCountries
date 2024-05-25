dbec21cd7453ab0b709de9bafbd13ab830b8c546
Jayesh <Jayesh@gmail.com>
2024-05-25 17:41:59 +0530

    fix 1

--- src/component/countries.jsx
+++ src/component/countries.jsx
@@ -1,3 +1,93 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import "./countries.style.css";
+
+// export default function Countries() {
+//   const [data, setData] = useState([]);
+//   const [search, setSearch] = useState("");
+//   const [timer, timerFn] = useState(null);
+//   const [listData, setListData] = useState([]);
+
+//   const debounceSearch = (event, debounceTimeout) => {
+//     clearTimeout(debounceTimeout);
+//     let tId = setTimeout(() => {
+//       console.log(event.target.value);
+//       setSearch(event.target.value);
+//     }, 500);
+//     timerFn(tId);
+//   };
+
+//   useEffect(() => {
+//     let NewArray = [];
+      
+//     data.forEach((ele) => {
+//           if(ele.name.common.toLowerCase().includes(search.toLowerCase())) {
+//               NewArray.push(ele)
+//             }
+//         })
+//     setListData([...NewArray])
+//   }, [search]);
+
+
+//   useEffect(() => {
+//     async function onload() {
+//       await axios
+//         .get("https://restcountries.com/v3.1/all")
+//         .then((data) => {
+//           console.log(data.data);
+//           setData(data.data);
+//           setListData(data.data)
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+//     }
+//     onload();
+//   }, []);
+
+//   const Card = ({ img, alt, name }) => {
+//     return (
+//       <div className="Card">
+//         <img src={img} alt={alt} className="CardImage" />
+//         <h3 className="CardName">{name}</h3>
+//       </div>
+//     );
+//   };
+
+
+//   return (
+//     <>
+//       <div className="nav">
+//         <input
+//           type="text"
+//           className="input"
+//           placeholder="Search for countries..."
+//           onChange={(e) => {
+//             debounceSearch(e, timer);
+//           }}
+//         />
+//       </div>
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           flexWrap: "wrap",
+//         }}
+//       >
+//         {listData.map((ele) => (
+//           <Card
+//             key={ele.car.cca3}
+//             img={ele.flags.png}
+//             alt={ele.flags.alt}
+//             name={ele.name.common}
+//           />
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./countries.style.css";
@@ -5,55 +95,47 @@ import "./countries.style.css";
 export default function Countries() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  const [timer, timerFn] = useState(null);
   const [listData, setListData] = useState([]);
 
-  const debounceSearch = (event, debounceTimeout) => {
-    clearTimeout(debounceTimeout);
-    let tId = setTimeout(() => {
-      console.log(event.target.value);
-      setSearch(event.target.value);
-    }, 500);
-    timerFn(tId);
+  // Debounce search input
+  const debounceSearch = (event) => {
+    const value = event.target.value;
+    setSearch(value);
   };
 
+  // Update filtered list based on search
   useEffect(() => {
-    let NewArray = [];
-      
-    data.forEach((ele) => {
-          if(ele.name.common.toLowerCase().includes(search.toLowerCase())) {
-              NewArray.push(ele)
-            }
-        })
-    setListData([...NewArray])
-  }, [search]);
+    if (search === "") {
+      setListData(data);
+    } else {
+      const filteredData = data.filter((ele) =>
+        ele.name.common.toLowerCase().includes(search.toLowerCase())
+      );
+      setListData(filteredData);
+    }
+  }, [search, data]);
 
-
+  // Load country data on component mount
   useEffect(() => {
     async function onload() {
-      await axios
-        .get("https://restcountries.com/v3.1/all")
-        .then((data) => {
-          console.log(data.data);
-          setData(data.data);
-          setListData(data.data)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        setData(response.data);
+        setListData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
     onload();
   }, []);
 
-  const Card = ({ img, alt, name }) => {
-    return (
-      <div className="Card">
-        <img src={img} alt={alt} className="CardImage" />
-        <h3 className="CardName">{name}</h3>
-      </div>
-    );
-  };
-
+  // Card component for each country
+  const Card = ({ img, alt, name }) => (
+    <div className="Card">
+      <img src={img} alt={alt} className="CardImage" />
+      <h3 className="CardName">{name}</h3>
+    </div>
+  );
 
   return (
     <>
@@ -62,9 +144,7 @@ export default function Countries() {
           type="text"
           className="input"
           placeholder="Search for countries..."
-          onChange={(e) => {
-            debounceSearch(e, timer);
-          }}
+          onChange={debounceSearch}
         />
       </div>
       <div
@@ -77,7 +157,7 @@ export default function Countries() {
       >
         {listData.map((ele) => (
           <Card
-            key={ele.car.cca3}
+            key={ele.cca3} // corrected key property
             img={ele.flags.png}
             alt={ele.flags.alt}
             name={ele.name.common}
